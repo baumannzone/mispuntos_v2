8eb2ea4ac428dedf20d1732f96a38b692a970a81
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-08-08 18:33:48 +0200

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,11 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
+    'computed-property-spacing': ['error', 'always'],
+    'space-in-parens': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'always'],
+    'comma-dangle': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
